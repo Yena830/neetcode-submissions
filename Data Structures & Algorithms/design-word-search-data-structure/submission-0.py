b131c5f072726fd2896857804c255d70696ed3e2
Yueyue Lin <160773767+Yena830@users.noreply.github.com>
2026-04-22 19:19:00 -0500

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.son = {}
+        self.end = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+        
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.son:
+                node.son[c] = TrieNode()
+            node = node.son[c]
+        node.end = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(i,root):
+            node = root
+            for j in range(i,len(word)):
+                if word[j] == '.':
+                    for child in node.son.values():
+                        if dfs(j+1,child):
+                            return True
+                    return False
+                else:
+                    if word[j] not in node.son:
+                        return False
+                    node = node.son[word[j]]
+            return node.end
+        return dfs(0,self.root)
+        

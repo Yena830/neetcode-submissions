b7be9463df7a1397deead9169ed2a71a1135b666
Yueyue Lin <160773767+Yena830@users.noreply.github.com>
2026-04-22 19:04:04 -0500

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.son = {}
+        self.end = False
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            # if c not show in Trie
+            if c not in node.son:
+                node.son[c] = TrieNode()
+            node = node.son[c]
+        # mark the end of the word
+        node.end = True
+
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.son:
+                return False
+            node = node.son[c]
+        return True if node.end else False
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.son:
+                return False
+            node = node.son[c]
+        return True
+        
+        
